1b385d12a48c4161fc484428804750fd12b3cae7
Noah Hoback <fullstack47@outlook.com>
2021-03-19 06:59:07 -0400

    added the js for first timeslot (block1)

--- script.js
+++ script.js
@@ -3,4 +3,8 @@ $("#todaysDate").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
 
 var currentTime = moment();
 currentTime = currentTime.startOf("hour");
-var before = moment().startOf("day").add(9, "hours")
+var before = moment().startOf("day").add(9, "hours");
+
+var timeSlot1 = before.add(0, "h");
+timeSlot1 = timeSlot1.format('hh:mm A');
+$(".block1").text(timeSlot1);
