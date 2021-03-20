2bf900fb214b61ee9fa876437b9e280e05746669
Noah Hoback <fullstack47@outlook.com>
2021-03-20 11:33:23 -0400

    code for function timeTest for timeSlots 2-9

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ var timeSlot9 = before.add(1, "h");
 $(".block9").text(timeSlot9); 
 
 function timeTest() {
-    time1 = moment().startOf('day').add(9, "hours");
+    timeSlot1 = moment().startOf('day').add(9, "hours");
     currentTime = currentTime.startOf("hour");
 
     if (currentTime.isAfter(timeSlot1)) {
@@ -52,6 +52,95 @@ function timeTest() {
     }else if (currentTime.isSame(timeSlot1)) {
         $(".form9").addClass("present");
     };
+    timeSlot2 = moment().startOf('day').add(10, "hours");
+currentTime = currentTime.startOf("hour");
+
+if (currentTime.isAfter(timeSlot2)) {
+    $(".form10").addClass("past");
+}
+else if (currentTime.isBefore(timeSlot2)) {
+    $(".form10").addClass("future");
+}
+else if (currentTime.isSame(timeSlot2)) {
+    $(".form10").addClass("present");
+};
+
+timeSlot3 = moment().startOf('day').add(11, "hours");
+currentTime = currentTime.startOf("hour");
+
+if (currentTime.isAfter(timeSlot3)) {
+$(".form11").addClass("past");
+}
+else if (currentTime.isBefore(timeSlot3)) {
+$(".form11").addClass("future");
+}
+else if (currentTime.isSame(timeSlot3)) {
+$(".form11").addClass("present");
+};
+
+timeSlot4 = moment().startOf('day').add(12, "hours");
+currentTime = currentTime.startOf("hour");
+
+if (currentTime.isAfter(timeSlot4)) {
+$(".form12").addClass("past");
+}
+else if (currentTime.isBefore(timeSlot4)) {
+$(".form12").addClass("future");
+}
+else if (currentTime.isSame(timeSlot4)) {
+$(".form12").addClass("present");
+};
+
+timeSlot5 = moment().startOf('day').add(13, "hours");
+currentTime = currentTime.startOf("hour");
+
+if (currentTime.isAfter(timeSlot5)) {
+$(".form1").addClass("past");
+}
+else if (currentTime.isBefore(timeSlot5)) {
+$(".form1").addClass("future");
+}
+else if (currentTime.isSame(timeSlot5)) {
+$(".form1").addClass("present");
+};
+
+timeSlot6 = moment().startOf('day').add(14, "hours");
+currentTime = currentTime.startOf("hour");
+
+if (currentTime.isAfter(timeSlot6)) {
+$(".form2").addClass("past");
+}
+else if (currentTime.isBefore(timeSlot6)) {
+$(".form2").addClass("future");
+}
+else if (currentTime.isSame(timeSlot6)) {
+$(".form2").addClass("present");
+};
+
+timeSlot7 = moment().startOf('day').add(15, "hours");
+currentTime = currentTime.startOf("hour");
+
+if (currentTime.isAfter(timeSlot7)) {
+$(".form3").addClass("past");
+}
+else if (currentTime.isBefore(timeSlot7)) {
+$(".form3").addClass("future");
+}
+else if (currentTime.isSame(timeSlot7)) {
+$(".form3").addClass("present");
+};
+
+timeSlot8 = moment().startOf('day').add(16, "hours");
+currentTime = currentTime.startOf("hour");
+
+if (currentTime.isAfter(timeSlot8)) {
+$(".form4").addClass("past");
+}
+else if (currentTime.isBefore(timeSlot8)) {
+$(".form4").addClass("future");
+}
+else if (currentTime.isSame(timeSlot8)) {
+$(".form4").addClass("present");
+};
     
-    }
 }
